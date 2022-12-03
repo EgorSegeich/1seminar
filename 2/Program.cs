f9c6c59b803ceba7befcc7bc4a4d6464ce373dfd
Egor <aldes@list.ru>
2022-12-03 16:14:48 +0300

    выполненное 1 и 2 задания

---
+++ 2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите первое числ: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int quotient = number1 / number2;
+
+if(quotient == number2)
+{
+Console.Write("Yes");
+}
+else
+{
+Console.Write("no");
+}
---
+++ 3/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("введите номер дня недели: ");
+int numberd = Convert.ToInt32(Console.ReadLine());
+
+if(numberd == 1)
+{
+Console.Write("понедельник");
+}
+
+if(numberd == 2)
+{
+Console.Write("вторник");
+}
+
+if(numberd == 3)
+{
+Console.Write("среда");
+}
+
+if(numberd == 4)
+{
+Console.Write("четверг");
+}
+
+if(numberd == 5)
+{
+Console.Write("пятница");
+}
+
+if(numberd == 6)
+{
+Console.Write("суббота");
+}
+
+if(numberd == 7)
+{
+Console.Write("воскресенье");
+}
+else
+{
+Console.Write("введите число от 1 до 7");
+}
