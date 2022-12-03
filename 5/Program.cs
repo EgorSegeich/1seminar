2203f4b057dce8210cd6d67474bb7643323ebff4
Egor <aldes@list.ru>
2022-12-03 17:59:42 +0300

    выполнено 1,2,3,4,5 заданияс дополнениями

--- 5/Program.cs
+++ 5/Program.cs
@@ -1,3 +1,9 @@
 ﻿Console.WriteLine("введите трёхзначное число: ");
+string N = Console.ReadLine();
+Console.WriteLine(N[N.Length - 1]);
+
+/*
+Console.WriteLine("введите трёхзначное число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(N%10);
+*/
